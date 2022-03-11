a37a7c554e9caf309253d447f164bf7eff59fc08
cesar <juniorariasrojas@gmail.com>
2022-03-11 01:56:37 -0500

    nav-bar v1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import NavBar from "./components/NavBar";
+
+
+function App() {
+  return (
+    <Fragment>
+      <NavBar />
+      <h2>CONTENIDO!</h2>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Menu, Image, Link } from 'antd';
+import { MobileOutlined, RobotOutlined, LaptopOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+
+const { SubMenu, Item } = Menu;
+
+const NavBar = () => {
+  return (
+    <header className='header'>
+        <div className='nav-main'>
+            <div className='left'>
+                    <a href="#" className='logo-main'>
+                        <img width={45} height={45} src="https://i.ibb.co/n3XV1GV/logoG.png" alt="logo" />
+                        <p>Gʌming<span>SHOP</span></p>
+                    </a>
+                </div>
+            <Menu mode='horizontal' className='center'>
+                <Item className='hover-item-nav' icon={<MobileOutlined className='icon-size'/>}>
+                        Celulares/Tables
+                </Item>
+                <Item className='hover-item-nav' icon={<RobotOutlined className='icon-size'/>} >
+                        Videojuegos
+                </Item>
+                <Item className='hover-item-nav' icon={<LaptopOutlined className='icon-size'/>} >
+                        Laptops/Pcs
+                </Item> 
+            </Menu>
+            <div className='right'>
+                <button className='btn-login'>Login</button>
+                
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default NavBar
