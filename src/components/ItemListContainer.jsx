b9b7b224360ce232e823abc8fc1739044fd93b51
cesar <juniorariasrojas@gmail.com>
2022-03-17 17:30:23 -0500

    correción nav-responsive y uso del map

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,8 +5,11 @@ const ItemListContainer = (props) => {
   return (
     <div className='itemListContainer'>
         <h1>Nuestros Productos</h1>
-        <h2>{props.datos[0].nombre} / {props.datos[0].imagen}</h2>
-        <h2>{props.datos[1].nombre} / {props.datos[1].imagen}</h2>
+        {
+          props.datos.map((item) => 
+          <h2 key={item.id}>{item.nombre} - {item.imagen}</h2>
+          )
+        }
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,67 @@
-import React from 'react';
-import { Menu, Image, Link } from 'antd';
-import { MobileOutlined, RobotOutlined, LaptopOutlined } from '@ant-design/icons';
+import React, {useState} from 'react';
+import { Menu, Button, Drawer} from 'antd';
+import { MobileOutlined, RobotOutlined, LaptopOutlined, MenuOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import CartWidget from './CartWidget';
 
 const { SubMenu, Item } = Menu;
 
 const NavBar = () => {
-  return (
+
+    const [visible, setVisible] = useState(false);
+    const verDrawer = () =>{
+        setVisible(true);
+    }
+    const onClose = () =>{
+        setVisible(false);
+    }
+    return (
     <header className='header'>
+        <nav className='nav-bar'>
+
+        
         <div className='nav-main'>
-            <div className='left'>
-                    <a href="# " className='logo-main'>
-                        <img width={45} height={45} src="https://i.ibb.co/n3XV1GV/logoG.png" alt="logo" />
-                        <p>Gʌming<span>SHOP</span></p>
-                    </a>
-                </div>
-            <Menu mode='horizontal' className='center'>
-                <Item className='hover-item-nav' icon={<MobileOutlined className='icon-size'/>}>
-                        Celulares/Tables
-                </Item>
-                <Item className='hover-item-nav' icon={<RobotOutlined className='icon-size'/>} >
-                        Videojuegos
-                </Item>
-                <Item className='hover-item-nav' icon={<LaptopOutlined className='icon-size'/>} >
-                        Laptops/Pcs
-                </Item> 
-            </Menu>
-            <div className='right'>
-                <button className='btn-login'>Login</button>
-                <CartWidget />
-            </div>
+            
+                    <div className='left'>
+                            <a href="# " className='logo-main'>
+                                <img className='img-logo' src="https://i.ibb.co/r0vbQ7F/logo-main.png" alt="logo" />
+                                <p>Gʌming<span>SHOP</span></p>
+                            </a>
+                    </div>
+                
+                    <Menu mode='horizontal' className='center'>
+                        <Item key='cel' className='hover-item-nav' icon={<MobileOutlined className='icon-size'/>}>
+                                Celulares/Tables
+                        </Item>
+                        <Item key='games' className='hover-item-nav' icon={<RobotOutlined className='icon-size'/>} >
+                                Videojuegos
+                        </Item>
+                        <Item key='pcs' className='hover-item-nav' icon={<LaptopOutlined className='icon-size'/>} >
+                                Laptops/Pcs
+                        </Item> 
+                    </Menu>
+
+                    <div className='button-center'>
+                        <Button onClick={verDrawer} className='btn-menu'><MenuOutlined className='icon-size' /></Button>  
+                        <Drawer title='Nuestras Categorías'  placement='right' onClose={onClose} visible={visible} > 
+                            <div className='drawer-contenido'>
+                                <a className='drawer-link' href="# "> <MobileOutlined /> Celulares/Tables</a>
+                                <a className='drawer-link' href="# "> <RobotOutlined /> Videojuegos</a>
+                                <a className='drawer-link' href="# "> <LaptopOutlined /> Laptops/Pcs</a>
+                                <button className='drawer-login'>Iniciar Sesión</button>
+                            </div>
+                        </Drawer>
+                    </div>
+    
+                     <div className='right'>
+                        <button className='btn-login'>Iniciar Sesión</button>
+                        <CartWidget />
+                    </div>
+
+                
+                
         </div>
+        </nav>
     </header>
   )
 }
